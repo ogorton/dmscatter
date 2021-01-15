ee90e0d00cfd54a3a090511904870af3b0ccb989
Oliver C. Gorton <ogorton@sdsu.edu>
2021-01-15 14:53:09 -0800

    updated py script

--- python/masscompare.py
+++ python/masscompare.py
@@ -7,22 +7,20 @@ import runCustomControl
 exec_name = "../src/darkmatter.x"
 
 # This script works by replacing MASS_KEYWORD with the desired value of the
-# WIMP mass, then running the code with a control file "si28.control".
+# WIMP mass, then running the code with a control file "<>.control".
 
 control_template = "xe131.control" # Filename ends in .template, this is the prefix.
 inputfile = 'input.xe131'
 workdir = os.getcwd()
 label = "xe131"
-# Where to find the resulting event rate spectra:
-resultfile = "eventrate_spectra.dat"
 
 plt.figure()
 
 for wimp_mass in (7., 50., 500.):
 
     param_dict = { "MASS_KEYWORD" : wimp_mass }
-    RecoilE, EventRate = runCustomControl.runCustomControl(exec_name, inputfile, control_template, param_dict,
-        workdir, label)
+    RecoilE, EventRate = runCustomControl.runCustomControl(exec_name, 
+            inputfile, control_template, param_dict, workdir, label)
 
     plt.plot(RecoilE, EventRate, label="$m_\chi=$%2.2f"%wimp_mass)
 
