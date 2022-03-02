344b23438495dd25f5cbece62c23959443841673
Oliver Gorton <46658128+ogorton@users.noreply.github.com>
2022-03-01 16:26:56 -0800

    alphabetize keywords in keywords.f90

--- src/keywords.f90
+++ src/keywords.f90
@@ -128,71 +128,17 @@ module keywords
     
         select case (keyword)
     
-        case('vearth')
-            vearth = keyvalue
-            print*,trim(keyword),": Set velocity of earth in galactic frame set to",vearth
-    
         case('dmdens')
             wimp%localdensity = keyvalue
             print*,trim(keyword),": Set local dark matter density to",keyvalue
     
-        case('quadtype')
-            quadrature_type = int(keyvalue)
-            print*,trim(keyword),': not implemented.'
-    
-        case('quadrelerr')
-            quadrature_relerr = dble( keyvalue )
-            print*,trim(keyword),": Set adaptive quadrature routine to seek relative error:",keyvalue
-    
-        case('gev')
-            gev = keyvalue
-            print*,trim(keyword),': Set GeV units to',gev
-            femtometer = 5.0677/GeV
-            print*,'femtometer updated'
-    
-        case('femtometer')
-            femtometer = keyvalue
-            print*,trim(keyword),': Set femtometer units to',femtometer
-    
-        case('wimpmass')
-            wimp%mass = keyvalue
-            print*,trim(keyword),': Set dark matter particle mass to',keyvalue
-    
-        case('vescape')
-            vescape = keyvalue
-            print*,trim(keyword),': Set escape velocity to', vescape
-    
-        case('weakmscale')
-            mV = keyvalue
-            print*,trim(keyword),': Set standard-model weak interaction mass scale to',mv
-    
-        case('maxwellv0')
-            vscale = keyvalue
-            print*,trim(keyword),': Set velocity distribution scaling to',vscale
-    
-        case('mnucleon')
-            mn = keyvalue
-            print*,trim(keyword),': Set nucleon mass to',mn
-    
         case('dmspin')
             wimp%j = keyvalue
             print*,trim(keyword),': Set dark matter particle spin to',keyvalue
-    
-        case('usemomentum')
-            if (keyvalue==1) then
-                usemomentum = .true.
-                print*,trim(keyword),': Set to use momentum transfer instead& 
-                        & of recoil energy'
-            else
-                usemomentum = .false.
-            end if
-    
-        case('useenergyfile')
-            if (keyvalue==1) then
-                useenergyfile = .true.
-                print*,trim(keyword),': Set to use recoil energies in file&
-                        & instead of linear grid specification.'
-            end if
+
+        case('femtometer')
+            femtometer = keyvalue
+            print*,trim(keyword),': Set femtometer units to',femtometer            
     
         case('fillnuclearcore')
             if (keyvalue==1) then
@@ -203,18 +149,36 @@ module keywords
                 print*,trim(keyword),': Set to leave denisty matrix core orbitals unfilled.'
             end if
 
-        case('ntscale')
-            ntscale = keyvalue
-            print*,trim(keyword),': Set experimental mass scaling to',keyvalue
-    
+        case('gaussorder')
+            gaussorder = int(keyvalue)
+            print*,trim(keyword),': Set order of Gauss-Legendre quadrature to',keyvalue
+
+        case('gev')
+            gev = keyvalue
+            print*,trim(keyword),': Set GeV units to',gev
+            femtometer = 5.0677/GeV
+            print*,'femtometer updated'            
+
         case('hofrequency')
             bfm = 6.43d0/sqrt(keyvalue) * femtometer
             print*,trim(keyword),': Set harmonic oscillator frequency to',keyvalue,'and b(1/GeV) to',bfm
-    
+
         case('hoparameter')
             bfm = keyvalue * femtometer
-            print*,trim(keyword),': Set harmonic oscillator parameter to',keyvalue,'and b(1/GeV) to',bfm
-    
+            print*,trim(keyword),': Set harmonic oscillator parameter to',keyvalue,'and b(1/GeV) to',bfm            
+
+        case('maxwellv0')
+            vscale = keyvalue
+            print*,trim(keyword),': Set velocity distribution scaling to',vscale
+
+        case('mnucleon')
+            mn = keyvalue
+            print*,trim(keyword),': Set nucleon mass to',mn            
+
+        case('ntscale')
+            ntscale = keyvalue
+            print*,trim(keyword),': Set experimental mass scaling to',keyvalue            
+
         case('printdensities')
             if (keyvalue==1) then
                 printdens = .true.
@@ -222,19 +186,55 @@ module keywords
             else
                 printdens = .false.
                 print*,trim(keyword),': Set to not print density matrix.'
-            end if        
-    
-        case('gaussorder')
-            gaussorder = int(keyvalue)
-            print*,trim(keyword),': Set order of Gauss-Legendre quadrature to',keyvalue
-    
-        case('sj2tablemin')
-            tablemin2j = int(keyvalue)
-            print*,trim(keyword),': Set minimum Wigner 6-J table value to Jx2 =',keyvalue
-    
+            end if            
+
+        case('quadrelerr')
+            quadrature_relerr = dble( keyvalue )
+            print*,trim(keyword),": Set adaptive quadrature routine to seek relative error:",keyvalue
+
+        case('quadtype')
+            quadrature_type = int(keyvalue)
+            print*,trim(keyword),': not implemented.'
+
         case('sj2tablemax')
             tablemax2j = int(keyvalue)
             print*,trim(keyword),': Set mmaximum Wigner 6-J table value to Jx2 =',keyvalue
+
+        case('sj2tablemin')
+            tablemin2j = int(keyvalue)
+            print*,trim(keyword),': Set minimum Wigner 6-J table value to Jx2 =',keyvalue
+
+        case('useenergyfile')
+            if (keyvalue==1) then
+                useenergyfile = .true.
+                print*,trim(keyword),': Set to use recoil energies in file&
+                        & instead of linear grid specification.'
+            end if            
+
+        case('usemomentum')
+            if (keyvalue==1) then
+                usemomentum = .true.
+                print*,trim(keyword),': Set to use momentum transfer instead& 
+                        & of recoil energy'
+            else
+                usemomentum = .false.
+            end if            
+
+        case('vearth')
+            vearth = keyvalue
+            print*,trim(keyword),": Set velocity of earth in galactic frame set to",vearth            
+    
+        case('vescape')
+            vescape = keyvalue
+            print*,trim(keyword),': Set escape velocity to', vescape
+
+        case('weakmscale')
+            mV = keyvalue
+            print*,trim(keyword),': Set standard-model weak interaction mass scale to',mv
+
+        case('wimpmass')
+            wimp%mass = keyvalue
+            print*,trim(keyword),': Set dark matter particle mass to',keyvalue
     
         case default
             print*,''
